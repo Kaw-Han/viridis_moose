c09f2dbb9be7713c493fcb0097b7c9ce2478df59
kaw_han <hanifk58@gmail.com>
2025-11-14 09:58:49 +0100

    created plot of penguins using Economist style (ggthemes)

---
+++ viridis_moosesnus.R
@@ -0,0 +1,23 @@
+
+library(ggplot2)
+library(palmerpenguins)
+library(ggthemes)
+
+
+#plot some viridis penguins
+
+ggplot(penguins, aes(x = species, y = bill_length_mm, fill = species)) +
+  geom_boxplot(alpha = 0.8) +
+  scale_fill_economist() +
+  labs(
+    title = "Bill Length of Penguin Species",
+    x = "Species",
+    y = "Bill Length (mm)"
+  ) +
+  theme_economist() +
+  theme(
+
+    plot.title = element_text(hjust = 0.5, face = "bold"),
+    legend.position = "none"
+  )
+
